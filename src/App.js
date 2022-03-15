5c33051cef3eae019f2792621571e06891a7c104
merlinpraksi <merlinpraksi@gmail.com>
2022-03-15 16:51:25 +0100

    com

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { Header } from "./components/Header";
-
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="container">
      
       <Header/>
-    
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Router>
     </div>
   );
 }
